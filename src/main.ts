104bb1a9a5f9767227fba579c7ab73c577165000
dstiscar <dstiscar@ucsc.edu>
2025-10-08 23:38:25 +0000

    Step 2 complete: added a counter that increments for every button click

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,18 @@ import catImageUrl from "./black-cat_1f408-200d-2b1b.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <button name="favorite">
+  <p>Care: <span id="counter">0</span></p>
+  <button id="catbutton">
   <img src="${catImageUrl}" class="icon" />
   </button>
 `;
+
+const button = document.getElementById("catbutton")!;
+const counterElement = document.getElementById("counter")!;
+
+let counter: number = 0;
+
+button.addEventListener("click", () => {
+  counter += 1;
+  counterElement.textContent = counter.toString();
+});
