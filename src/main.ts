ab0de96669e67c4403dfd42f4a8172430c42cb0c
dstiscar <dstiscar@ucsc.edu>
2025-10-08 18:56:38 +0000

    changed image asset text

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <p>This is a PAPERCLIP: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
