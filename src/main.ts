4997457d92155aac9565ba52ef55825bbdcea005
dstiscar <dstiscar@ucsc.edu>
2025-10-08 23:27:25 +0000

    Step 1 complete: added a button with an emoji on it

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+import catImageUrl from "./black-cat_1f408-200d-2b1b.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>This is a PAPERCLIP: <img src="${exampleIconUrl}" class="icon" /></p>
+  <button name="favorite">
+  <img src="${catImageUrl}" class="icon" />
+  </button>
 `;
