bd8a21b84776a6ad8d3c9382b319a45a3783e8dc
dstiscar <dstiscar@ucsc.edu>
2025-10-09 19:46:13 +0000

    Step 5 complete: added a new button that allows you to purchase upgrades

--- src/main.ts
+++ src/main.ts
@@ -6,29 +6,52 @@ document.body.innerHTML = `
   <button id="catbutton">
   <img src="${catImageUrl}" class="icon" />
   </button>
+  <button id="tlcbutton">
+  TLC (10 Care)
+  </button>
 `;
 
-const button = document.getElementById("catbutton")!;
+const catButton = document.getElementById("catbutton")! as HTMLButtonElement;
+const tlcButton = document.getElementById("tlcbutton")! as HTMLButtonElement;
+tlcButton.disabled = true;
 const counterElement = document.getElementById("counter")!;
 
 let startTime: number = 0;
-const interval = 1000;
+let interval: number = 1000;
+let growthRate: number = 0;
+const tlcCost = 10;
 
 let counter: number = 0;
 
 function myCallback(timestamp: number) {
   if (!startTime) startTime = timestamp;
-  if (timestamp - startTime >= interval) {
-    counter += 1;
-    counterElement.textContent = counter.toString();
-    startTime = timestamp;
+
+  interval = 1000 / growthRate;
+  if (growthRate > 0) {
+    if (timestamp - startTime >= interval) {
+      counter += 1;
+      counterElement.textContent = counter.toString();
+      tlcButton.disabled = counter < tlcCost;
+      startTime = timestamp;
+    }
   }
   requestAnimationFrame(myCallback);
 }
 
 requestAnimationFrame(myCallback);
 
-button.addEventListener("click", () => {
+catButton.addEventListener("click", () => {
   counter += 1;
   counterElement.textContent = counter.toString();
+  tlcButton.disabled = counter < tlcCost;
 });
+
+tlcButton.addEventListener("click", () => {
+  counter -= tlcCost;
+  growthRate += 1;
+  counterElement.textContent = counter.toString();
+  tlcButton.disabled = true;
+  startTime = 0;
+});
+
+
