5329911636569e9665d22d9919131504a1157755
dstiscar <dstiscar@ucsc.edu>
2025-10-12 01:05:17 +0000

    Step 9 complete: refactored code using interfaces

--- src/main.ts
+++ src/main.ts
@@ -27,35 +27,54 @@ document.body.innerHTML = `
   </p>
 `;
 
+interface Item {
+  name: string;
+  cost: number;
+  rate: number;
+  amount: number;
+  button: HTMLButtonElement;
+  amtelement: Element;
+  costelement: Element;
+}
+
 const catButton = document.getElementById("catbutton")! as HTMLButtonElement;
 const counterElement = document.getElementById("counter")!;
 const rateElement = document.getElementById("rate")!;
 
-const handButton = document.getElementById("handbutton")! as HTMLButtonElement;
-const tlcButton = document.getElementById("tlcbutton")! as HTMLButtonElement;
-const mechButton = document.getElementById("mechbutton")! as HTMLButtonElement;
-
-const handamtElement = document.getElementById("handamt")!;
-const tlcamtElement = document.getElementById("tlcamt")!;
-const mechamtElement = document.getElementById("mechamt")!;
-
-const handcostElement = document.getElementById("handcost")!;
-const tlccostElement = document.getElementById("tlccost")!;
-const mechcostElement = document.getElementById("mechcost")!;
-
-let handCost: number = 10;
-let tlcCost: number = 100;
-let mechCost: number = 1000;
-
-let handAmt: number = 0;
-let tlcAmt: number = 0;
-let mechAmt: number = 0;
-
-let startTime: number = 0;
-let fps: number = 0;
-let growthRate: number = 0;
+const Items: Item[] = [
+  {
+    name: "Extra Hand",
+    cost: 10,
+    rate: 0.1,
+    amount: 0,
+    button: document.getElementById("handbutton")! as HTMLButtonElement,
+    amtelement: document.getElementById("handamt")!,
+    costelement: document.getElementById("handcost")!,
+  },
+  {
+    name: "TLC",
+    cost: 100,
+    rate: 2,
+    amount: 0,
+    button: document.getElementById("tlcbutton")! as HTMLButtonElement,
+    amtelement: document.getElementById("tlcamt")!,
+    costelement: document.getElementById("tlccost")!,
+  },
+  {
+    name: "Petting Machine",
+    cost: 1000,
+    rate: 50,
+    amount: 0,
+    button: document.getElementById("mechbutton")! as HTMLButtonElement,
+    amtelement: document.getElementById("mechamt")!,
+    costelement: document.getElementById("mechcost")!,
+  },
+];
 
 let counter: number = 0;
+let growthRate: number = 0;
+let startTime: number = 0;
+let fps: number = 0;
 
 function myCallback(
   timestamp: number = performance.timeOrigin + performance.now(),
@@ -69,49 +88,25 @@ function myCallback(
   counterElement.textContent = Math.floor(counter).toString();
   rateElement.textContent = (Math.floor(growthRate * 10) / 10).toString();
 
-  handamtElement.textContent = Math.floor(handAmt).toString();
-  tlcamtElement.textContent = Math.floor(tlcAmt).toString();
-  mechamtElement.textContent = Math.floor(mechAmt).toString();
-
-  handcostElement.textContent = Math.floor(handCost).toString();
-  tlccostElement.textContent = Math.floor(tlcCost).toString();
-  mechcostElement.textContent = Math.floor(mechCost).toString();
-
-  handButton.disabled = counter < Math.floor(handCost);
-  tlcButton.disabled = counter < Math.floor(tlcCost);
-  mechButton.disabled = counter < Math.floor(mechCost);
+  Items.forEach((Item: Item) => {
+    Item.amtelement.textContent = Math.floor(Item.amount).toString();
+    Item.costelement.textContent = Math.floor(Item.cost).toString();
+    Item.button.disabled = counter < Math.floor(Item.cost);
+  });
 
   requestAnimationFrame(myCallback);
 }
-
 requestAnimationFrame(myCallback);
 
 catButton.addEventListener("click", () => {
-  counter += 1;
-  counterElement.textContent = counter.toString();
-  handButton.disabled = counter < handCost;
+  counter++;
 });
 
-handButton.addEventListener("click", () => {
-  growthRate += 0.1;
-
-  counter -= handCost;
-  handAmt += 1;
-  handCost *= 1.15;
-});
-
-tlcButton.addEventListener("click", () => {
-  growthRate += 2;
-
-  counter -= tlcCost;
-  tlcAmt += 1;
-  tlcCost *= 1.15;
-});
-
-mechButton.addEventListener("click", () => {
-  growthRate += 50;
-
-  counter -= mechCost;
-  mechAmt += 1;
-  mechCost *= 1.15;
+Items.forEach((Item: Item) => {
+  Item.button.addEventListener("click", () => {
+    growthRate += Item.rate;
+    counter -= Item.cost;
+    Item.amount++;
+    Item.cost *= 1.15;
+  });
 });
